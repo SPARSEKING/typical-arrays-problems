6c18fab089a7601122e4974336b9316f9044e058
Kirill <gavrilchikkirill6@gmail.com>
2020-09-28 21:20:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array === undefined || array[0] === undefined){
+    return 0;
+  }
+    let variable = array[0];
+    for (let i = 0; i <= array.length; i++){
+        if (array[i] < variable){
+          variable = array[i];
+        }
+      }
+      return variable;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array === undefined || array[0] === undefined){
+    return 0;
+  }
+    let variable = array[0];
+    for (let i = 0; i <= array.length; i++){
+        if(array[i] > variable){
+          variable = array[i];
+        }
+    }
+    return variable;
+  
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+
+  if (array === undefined || array[0] === undefined){
+    return 0;
+  }
+  let sum = 0;
+    for (let i = 0; i < array.length; i++){
+      sum += array[i];
+    }
+  let average = sum / array.length;
+  return average;
+};
+
